16cbde2e9568472dbf71410ac287c2ce04e435a5
Moses Williams <mowi6854@colorado.edu>
2025-04-19 10:17:26 -0600

    Adding git ignore to only focus on Core

--- Core/Inc/gameplay.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GAMEPLAY_H
-#define __GAMEPLAY_H
-#include "LCD_Driver.h"
-#include "stm32f4xx_hal.h"
-
-void runGameplay(uint8_t gameMode);
-
-
-#endif
--- Debug/Core/Src/Text.d
+++
@@ -1,2 +0,0 @@
-Core/Src/Text.o: ../Core/Src/Text.c ../Core/Inc/Text.h
-../Core/Inc/Text.h:
